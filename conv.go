e53506f9ed72e2fa56457dbd8882311da800eb32
Pouya Vedadiyan <vedadiyan@gmail.com>
2026-08-15 13:31:25 +0800

    quick fix and map added

--- conv.go
+++ conv.go
@@ -184,3 +184,27 @@ func ConvertFor[T any, R any](in *T) (*R, error) {
 	}
 	return out.Interface().(*R), nil
 }
+
+func FindConverter(l Type, r Type) (Converter, bool) {
+	lVal, ok := converters[l]
+	if !ok {
+		return nil, false
+	}
+	rVal, ok := lVal[r]
+	if !ok {
+		return nil, false
+	}
+	return rVal, true
+}
+
+func TargetFieldName(field StructField) string {
+	if val, ok := field.Tag.Lookup("mapto"); ok {
+		return val
+	}
+	return field.Name
+}
+
+func Zero[T any]() T {
+	var out T
+	return out
+}
