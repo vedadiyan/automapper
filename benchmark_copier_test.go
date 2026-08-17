ba0411135e6a947355f5f05a40339a1975b6af2e
Pouya Vedadiyan <Vedadiyan@gmail.com>
2026-08-18 00:31:37 +0800

    Codecs

---
+++ benchmark_copier_test.go
@@ -0,0 +1,131 @@
+package mapper
+
+import (
+	"testing"
+
+	"github.com/jinzhu/copier"
+)
+
+func BenchmarkAutomapper_Struct(b *testing.B) {
+	src := benchSourceValue
+
+	converter := CodecFor[benchSource, benchTarget]()
+
+	b.ReportAllocs()
+
+	for b.Loop() {
+		result, err := converter(&src)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = result
+	}
+}
+
+func BenchmarkCopier_Struct(b *testing.B) {
+	src := benchSourceValue
+
+	b.ReportAllocs()
+
+	for b.Loop() {
+		var dst benchTarget
+
+		if err := copier.Copy(&dst, &src); err != nil {
+			b.Fatal(err)
+		}
+		_ = dst
+	}
+}
+
+func BenchmarkAutomapper_NestedStruct(b *testing.B) {
+	src := benchNestedValue
+
+	converter := CodecFor[benchNestedSource, benchNestedTarget]()
+
+	b.ReportAllocs()
+
+	for b.Loop() {
+		result, err := converter(&src)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = result
+	}
+}
+
+func BenchmarkCopier_NestedStruct(b *testing.B) {
+	src := benchNestedValue
+
+	b.ReportAllocs()
+
+	for b.Loop() {
+		var dst benchNestedTarget
+
+		if err := copier.Copy(&dst, &src); err != nil {
+			b.Fatal(err)
+		}
+		_ = dst
+	}
+}
+
+func BenchmarkAutomapper_Slice(b *testing.B) {
+	src := benchSliceValue
+
+	converter := CodecFor[benchSliceSource, benchSliceTarget]()
+
+	b.ReportAllocs()
+
+	for b.Loop() {
+		result, err := converter(&src)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = result
+	}
+}
+
+func BenchmarkCopier_Slice(b *testing.B) {
+	src := benchSliceValue
+
+	b.ReportAllocs()
+
+	for b.Loop() {
+		var dst benchSliceTarget
+
+		if err := copier.Copy(&dst, &src); err != nil {
+			b.Fatal(err)
+		}
+		_ = dst
+	}
+}
+
+func BenchmarkAutomapper_Map(b *testing.B) {
+	src := benchMapValue
+
+	converter := CodecFor[benchMapSource, benchMapTarget]()
+
+	b.ReportAllocs()
+
+	for b.Loop() {
+		result, err := converter(&src)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = result
+	}
+}
+
+func BenchmarkCopier_Map(b *testing.B) {
+	src := benchMapValue
+
+	b.ReportAllocs()
+
+	for b.Loop() {
+		var dst benchMapTarget
+
+		if err := copier.Copy(&dst, &src); err != nil {
+			b.Fatal(err)
+		}
+		_ = dst
+	}
+}
---
+++ codec.go
@@ -0,0 +1,191 @@
+package mapper
+
+import (
+	"reflect"
+)
+
+type (
+	CodecFn func(sourceField RType, targetField RType) func(sourceValue RValue, targetValue RValue) error
+)
+
+var (
+	codecs []CodecFn
+)
+
+func init() {
+	codecs = []CodecFn{
+		AssignCodec,
+		ConvertCodec,
+		StructCodec,
+		ArrayCodec,
+		MapCodec,
+	}
+}
+
+func FastConvertCodec(sourceField RType, targetField RType) func(sourceValue RValue, targetValue RValue) error {
+	outType := targetField.ConcreteType()
+	return func(sourceValue RValue, targetValue RValue) error {
+		if sourceValue.Kind() != reflect.Pointer {
+			sourceValue = sourceValue.Reference(1)
+		}
+		out := NewAt(outType, sourceValue.ConcreteValue().Reference(1).UnsafePointer())
+		targetValue.SetAt(out.ConcreteValue(), targetValue.ptrCount)
+		return nil
+	}
+}
+
+func AssignCodec(sourceField RType, targetField RType) func(sourceValue RValue, targetValue RValue) error {
+	if !sourceField.AssignableTo(targetField) {
+		return nil
+	}
+
+	return func(sourceValue RValue, targetValue RValue) error {
+		targetValue.SetAt(sourceValue, targetField.PointerCount())
+		return nil
+	}
+}
+
+func ConvertCodec(sourceField RType, targetField RType) func(sourceValue RValue, targetValue RValue) error {
+	if !sourceField.ConvertibleTo(targetField) {
+		return nil
+	}
+
+	return func(sourceValue RValue, targetValue RValue) error {
+		targetValue.SetAt(valueOf(sourceValue.Convert(targetField.GoType())), targetField.PointerCount())
+		return nil
+	}
+}
+
+func StructCodec(sourceField RType, targetField RType) func(sourceValue RValue, targetValue RValue) error {
+	if sourceField.Kind() != reflect.Struct || targetField.Kind() != reflect.Struct {
+		return nil
+	}
+
+	out := make([]func(sourceValue RValue, targetValue RValue) error, 0)
+
+	for i := range sourceField.NumField() {
+		f := sourceField.Field(i)
+		target, ok := targetField.FieldByName(f.Name)
+		if !ok {
+			continue
+		}
+		codec := Codec(typeOf(f.Type).ConcreteType(), typeOf(target.Type).ConcreteType())
+
+		if codec == nil {
+			continue
+		}
+		out = append(out, func(sourceValue RValue, targetValue RValue) error {
+			err := codec(valueOf(sourceValue.Field(i)), valueOf(targetValue.FieldByName(f.Name)))
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+
+	}
+
+	return func(sourceValue, targetValue RValue) error {
+		for _, o := range out {
+			if err := o(sourceValue, targetValue); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
+func ArrayCodec(sourceField RType, targetField RType) func(sourceValue RValue, targetValue RValue) error {
+	if (sourceField.Kind() != reflect.Slice && sourceField.Kind() != reflect.Array) || (targetField.Kind() != reflect.Slice && targetField.Kind() != reflect.Array) {
+		return nil
+	}
+
+	targetElem := typeOf(targetField.Elem())
+	targetType := targetElem.ConcreteType()
+	codec := Codec(typeOf(sourceField.Elem()).ConcreteType(), targetType)
+	n := targetElem.PointerCount()
+
+	return func(sourceValue, targetValue RValue) error {
+		if targetField.Kind() == reflect.Slice {
+			if !sourceValue.IsZero() {
+				targetValue.SetAt(valueOf(reflect.MakeSlice(targetField.GoType(), sourceValue.Len(), sourceValue.Cap())), targetValue.PointerCount())
+			}
+		}
+		targetValue = targetValue.Refresh().ConcreteValue()
+		for i := range sourceValue.Len() {
+			target := valueOf(reflect.New(targetType.GoType()).Elem())
+			src := valueOf(sourceValue.Index(i))
+			err := codec(src, target)
+			if err != nil {
+				return err
+			}
+			targetValue.Index(i).Set(target.Reference(n).Value)
+		}
+		return nil
+	}
+}
+
+func MapCodec(sourceField RType, targetField RType) func(sourceValue RValue, targetValue RValue) error {
+	if sourceField.Kind() != reflect.Map || targetField.Kind() != reflect.Map {
+		return nil
+	}
+
+	targetKeyRawType := typeOf(targetField.Key())
+	targetValueRawType := typeOf(targetField.Elem())
+
+	sourceKeyRawType := typeOf(sourceField.Key())
+	sourceValueRawType := typeOf(sourceField.Elem())
+
+	keyType := targetKeyRawType.ConcreteType()
+	valueType := targetValueRawType.ConcreteType()
+
+	keyN := targetKeyRawType.PointerCount()
+	valueN := targetValueRawType.PointerCount()
+
+	keyCodec := Codec(sourceKeyRawType.ConcreteType(), keyType.ConcreteType())
+	valueCodec := Codec(sourceValueRawType.ConcreteType(), valueType.ConcreteType())
+
+	return func(sourceValue, targetValue RValue) error {
+		mapRange := sourceValue.MapRange()
+		targetValue.Set(reflect.MakeMap(targetField.GoType()))
+
+		for mapRange.Next() {
+			key := valueOf(New(keyType).Elem())
+			if err := keyCodec(valueOf(mapRange.Key()).ConcreteValue(), key); err != nil {
+				return err
+			}
+			value := valueOf(New(valueType).Elem())
+			if err := valueCodec(valueOf(mapRange.Value()).ConcreteValue(), value); err != nil {
+
+				return err
+			}
+			targetValue.SetMapIndex(key.Reference(keyN).Value, value.Reference(valueN).Value)
+		}
+		return nil
+	}
+}
+
+func Codec(src RType, target RType) func(sourceValue RValue, targetValue RValue) error {
+	for _, p := range codecs {
+		if fn := p(src, target); fn != nil {
+			return fn
+		}
+	}
+	return nil
+}
+
+func CodecFor[T any, R any]() func(in *T) (*R, error) {
+	src := TypeFor[T]()
+	target := TypeFor[R]()
+
+	fn := Codec(src.ConcreteType(), target.ConcreteType())
+	if fn == nil {
+		return nil
+	}
+	return func(in *T) (*R, error) {
+		targetValue := New(target.ConcreteType())
+		if err := fn(ValueOf(in).ConcreteValue(), targetValue.ConcreteValue()); err != nil {
+			return nil, err
+		}
+		return targetValue.Reference(target.PointerCount()).Interface().(*R), nil
+	}
+}
